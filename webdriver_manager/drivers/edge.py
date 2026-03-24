b88fba9e2e80a5cbd7b6af9faa46b8c88cf39d8a
Andrew <andrew.anggada@meratusline.com>
2026-03-24 18:44:06 +0700

    refactor(edge): use stable EdgeDriver release endpoint

--- webdriver_manager/drivers/edge.py
+++ webdriver_manager/drivers/edge.py
@@ -23,32 +23,9 @@ class EdgeChromiumDriver(Driver):
             os_system_manager
         )
 
-    def get_stable_release_version(self):
-        """Stable driver version when browser version was not determined."""
-        stable_url = self._latest_release_url.replace("LATEST_RELEASE", "LATEST_STABLE")
-        resp = self._http_client.get(url=stable_url)
-        return resp.text.rstrip()
-
     def get_latest_release_version(self) -> str:
-        determined_browser_version = self.get_browser_version_from_os()
-        log(f"Get LATEST {self._name} version for Edge {determined_browser_version}")
-
-        edge_driver_version_to_download = (
-            self.get_stable_release_version()
-            if (determined_browser_version is None)
-            else determined_browser_version
-        )
-        major_edge_version = edge_driver_version_to_download.split(".")[0]
-        os_type = self._os_system_manager.get_os_type()
-        latest_release_url = {
-            OSType.WIN
-            in os_type: f"{self._latest_release_url}_{major_edge_version}_WINDOWS",
-            OSType.MAC
-            in os_type: f"{self._latest_release_url}_{major_edge_version}_MACOS",
-            OSType.LINUX
-            in os_type: f"{self._latest_release_url}_{major_edge_version}_LINUX",
-        }[True]
-        resp = self._http_client.get(url=latest_release_url)
+        """Get latest stable EdgeDriver version."""
+        resp = self._http_client.get(url=self._latest_release_url)
         return resp.text.rstrip()
 
     def get_browser_type(self):
